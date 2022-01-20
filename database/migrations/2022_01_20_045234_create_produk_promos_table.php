e97831ac728799b460a4b996b1de3ac7e5f580fc
“MuhammadRidwanDiansyah” <ridwandiansyah3@gmail.com>
2022-01-20 13:39:00 +0700

    Model dan migrasi Produk Promo

--- database/migrations/2022_01_20_045234_create_produk_promos_table.php
+++ database/migrations/2022_01_20_045234_create_produk_promos_table.php
@@ -13,8 +13,16 @@ class CreateProdukPromosTable extends Migration
      */
     public function up()
     {
-        Schema::create('produk_promos', function (Blueprint $table) {
-            $table->bigIncrements('id');
+        Schema::create('produk_promo', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('produk_id')->unsigned();
+            $table->decimal('harga_awal', 16,2)->default(0);
+            $table->decimal('harga_akhir', 16,2)->default(0);
+            $table->integer('diskon_persen')->default(0);
+            $table->decimal('diskon_nominal', 16,2)->default(0);
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('produk_id')->references('id')->on('produk');
             $table->timestamps();
         });
     }
@@ -26,6 +34,6 @@ class CreateProdukPromosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('produk_promos');
+        Schema::dropIfExists('produk_promo');
     }
 }
