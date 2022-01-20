498a338c92659f4dae1a12057a98e071d7d045b1
“MuhammadRidwanDiansyah” <ridwandiansyah3@gmail.com>
2022-01-20 11:55:26 +0700

    Menambahkan Produk Promo

---
+++ app/Http/Controllers/ProdukPromoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ProdukPromo;
+use Illuminate\Http\Request;
+
+class ProdukPromoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ProdukPromo  $produkPromo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProdukPromo $produkPromo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ProdukPromo  $produkPromo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProdukPromo $produkPromo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ProdukPromo  $produkPromo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProdukPromo $produkPromo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ProdukPromo  $produkPromo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProdukPromo $produkPromo)
+    {
+        //
+    }
+}
