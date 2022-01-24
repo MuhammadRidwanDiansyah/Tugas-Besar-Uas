b2e93492412694d599288925daa74df553e2b3b1
“MuhammadRidwanDiansyah” <ridwandiansyah3@gmail.com>
2022-01-24 08:12:11 +0700

    Make Alamat Pengiriman dan Order

---
+++ app/Http/Controllers/AlamatPengirimanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\AlamatPengiriman;
+use Illuminate\Http\Request;
+
+class AlamatPengirimanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\AlamatPengiriman  $alamatPengiriman
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AlamatPengiriman $alamatPengiriman)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\AlamatPengiriman  $alamatPengiriman
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AlamatPengiriman $alamatPengiriman)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\AlamatPengiriman  $alamatPengiriman
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AlamatPengiriman $alamatPengiriman)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\AlamatPengiriman  $alamatPengiriman
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AlamatPengiriman $alamatPengiriman)
+    {
+        //
+    }
+}
