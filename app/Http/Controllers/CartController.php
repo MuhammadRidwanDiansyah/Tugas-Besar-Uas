e8ff17618b2a45cc868bdb9494328b637370e33e
“MuhammadRidwanDiansyah” <ridwandiansyah3@gmail.com>
2022-01-25 08:29:56 +0700

    Views checkout dan function index TransaksiController

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Cart;
-
+use App\AlamatPengiriman;
+use App\Order;
 class CartController extends Controller
 {
     /**
@@ -14,13 +15,17 @@ class CartController extends Controller
      */
     public function index(Request $request)
     {
-        $itemuser = $request->user(); //ambil data user
+        $itemuser = $request->user();//ambil data user
         $itemcart = Cart::where('user_id', $itemuser->id)
                         ->where('status_cart', 'cart')
                         ->first();
-        $data = array('title' => 'Shopping Cart',
-                    'itemcart' => $itemcart);
-        return view('cart.index', $data)->with('no', 1);
+        if ($itemcart) {
+            $data = array('title' => 'Shopping Cart',
+                        'itemcart' => $itemcart);
+            return view('cart.index', $data)->with('no', 1);            
+        } else {
+            return abort('404');
+        }
     }
 
     /**
@@ -75,7 +80,7 @@ class CartController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        
     }
 
     /**
@@ -91,8 +96,26 @@ class CartController extends Controller
 
     public function kosongkan($id) {
         $itemcart = Cart::findOrFail($id);
-        $itemcart->detail()->delete(); //hapus semua item di cart detail
+        $itemcart->detail()->delete();//hapus semua item di cart detail
         $itemcart->updatetotal($itemcart, '-'.$itemcart->subtotal);
         return back()->with('success', 'Cart berhasil dikosongkan');
+    }
+
+    public function checkout(Request $request) {
+        $itemuser = $request->user();
+        $itemcart = Cart::where('user_id', $itemuser->id)
+                        ->where('status_cart', 'cart')
+                        ->first();
+        $itemalamatpengiriman = AlamatPengiriman::where('user_id', $itemuser->id)
+                                                ->where('status', 'utama')
+                                                ->first();
+        if ($itemcart) {
+            $data = array('title' => 'Checkout',
+                        'itemcart' => $itemcart,
+                        'itemalamatpengiriman' => $itemalamatpengiriman);
+            return view('cart.checkout', $data)->with('no', 1);
+        } else {
+            return abort('404');
+        }
     }
 }
